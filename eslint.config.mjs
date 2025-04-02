97cdf29e0815f75fb9d5f587e81fde066eee2e27
Dan <dh_shin@caredoc.kr>
2025-04-02 11:38:06 +0900

    eslint 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import eslintConfigPrettier from "eslint-config-prettier/flat";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import unusedImports from "eslint-plugin-unused-imports";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -15,7 +16,10 @@ const compat = new FlatCompat({
 // files 옵션 제거 후 다시 실행
 export default tseslint.config(
   eslint.configs.recommended,
+  react.configs.flat.recommended,
   tseslint.configs.recommended,
+  jsxA11y.flatConfigs.recommended,
+  eslintPluginUnicorn.configs.recommended,
   eslintConfigPrettier,
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
@@ -39,7 +43,9 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "jsx-a11y": jsxA11y,
       "unused-imports": unusedImports,
+      unicorn: eslintPluginUnicorn,
     },
+
     rules: {
       "@typescript-eslint/no-explicit-any": 0,
       "react/jsx-curly-brace-presence": [
@@ -69,6 +75,8 @@ export default tseslint.config(
       ],
       "unused-imports/no-unused-imports": "error",
       "@typescript-eslint/no-unused-vars": "warn",
+      "react/no-unknown-property": ["error", { ignore: [] }],
+      "unicorn/consistent-function-scoping": "warn",
     },
   },
 );
