885c8d9a09e1e0efc09904f8dc722502fc1d2d4a
Dan <dh_shin@caredoc.kr>
2025-07-17 12:10:29 +0900

    chore: eslint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import eslint from "@eslint/js";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import tailwindcss from "eslint-plugin-tailwindcss";
 import unusedImports from "eslint-plugin-unused-imports";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -16,6 +18,8 @@ export default tseslint.config(
   eslint.configs.recommended,
   react.configs.flat.recommended,
   tseslint.configs.recommended,
+  tailwindcss.configs.recommended,
+  ...pluginQuery.configs["flat/recommended"],
   eslintConfigPrettier,
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
--- src/app/(layout)/page.tsx
+++ src/app/(layout)/page.tsx
@@ -1,5 +1,5 @@
 "use client";
 
 export default function Home() {
-  return <div>hello world</div>;
+  return <div className="table text-2xl">hello world</div>;
 }
